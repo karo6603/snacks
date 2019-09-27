e2fa35118f0acf1c288d0e887730e6de76a8cea8
Karoline Krogsbøll <you@example.com>
2019-09-27 17:33:15 +0200

    done

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+const form = document.querySelector("form#addForm");
+const formEdit = document.querySelector("form#editForm");
+form.setAttribute("novalidate", true);
+
+form.elements.snackName.addEventListener("focus", e => {
+  form.elements.snackName.classList.remove("notValid");
+});
+form.elements.snackName.addEventListener("blur", e => {
+  if (form.elements.snackName.checkValidity()) {
+    form.elements.snackName.classList.remove("notValid");
+  } else form.elements.snackName.classList.add("notValid");
+});
+
+form.addEventListener("submit", evt => {
+  post();
+  evt.preventDefault();
+});
+
+formEdit.addEventListener("submit", evt => {
+  evt.preventDefault();
+  put();
+});
+
 function get() {
   fetch("https://frontend2019-7c5c.restdb.io/rest/snacks", {
     method: "get",
@@ -26,23 +49,53 @@ function addSnackToTheDOM(snack) {
   copy.querySelector("p").textContent = snack.price;
   copy.querySelector(".rating").textContent = snack.rating;
 
-  copy.querySelector("button").addEventListener("click", () => {
-    deleteSnack(snack._id);
+  copy.querySelector("button.btnDelete").addEventListener("click", e => {
+    const target = e.target.parentElement;
+    target.classList.add("remove");
+
+    e.target.parentElement.addEventListener("transitionend", e => {
+      deleteSnack(snack._id);
+      if (e.propertyName == "opacity") {
+        target.remove();
+      }
+    });
+  });
+
+  copy.querySelector("button.btnEdit").addEventListener("click", e => {
+    fetchAndPopulate(snack._id);
   });
 
   document.querySelector("#app").prepend(copy);
 }
 
-const form = document.querySelector("form");
-console.log(form.elements);
+function fetchAndPopulate(id) {
+  fetch(`https://frontend2019-7c5c.restdb.io/rest/snacks/${id}`, {
+    method: "get",
+    headers: {
+      "Content-Type": "application/json; charset=uft-8",
+      "x-apikey": "5d887741fd86cb75861e260a",
+      "cache-control": "no-cache"
+    }
+  })
+    .then(e => e.json())
+    .then(snacks => {
+      formEdit.elements.snackName.value = snacks.name;
+      formEdit.elements.snackTaste.value = snacks.taste;
+      formEdit.elements.snackPrice.value = snacks.price;
+      formEdit.elements.snackRating.value = snacks.rating;
+      formEdit.elements.id.value = snacks._id;
+    });
+}
 
 function post() {
   const data = {
-    name: "chocolate",
-    taste: "sweet",
-    price: 25,
-    rating: 4
+    name: form.elements.snackName.value,
+    taste: form.elements.snackTaste.value,
+    price: form.elements.snackPrice.value,
+    rating: form.elements.snackRating.value
   };
+
+  addSnackToTheDOM(data);
 
   const postData = JSON.stringify(data);
   fetch("https://frontend2019-7c5c.restdb.io/rest/snacks", {
@@ -57,13 +110,44 @@ function post() {
     .then(res => res.json())
     .then(data => {
       console.log(data);
+    });
+}
 
-      addSnackToTheDOM(data);
+function put() {
+  const data = {
+    name: formEdit.elements.snackName.value,
+    taste: formEdit.elements.snackTaste.value,
+    price: formEdit.elements.snackPrice.value,
+    rating: formEdit.elements.snackRating.value
+  };
+
+  let postData = JSON.stringify(data);
+  const snackId = formEdit.elements.id.value;
+
+  fetch("https://frontend2019-7c5c.restdb.io/rest/snacks/" + snackId, {
+    method: "put",
+    headers: {
+      "content-type": "application/json; charset=utf-8",
+      "x-apikey": "5d887741fd86cb75861e260a",
+      "cache-control": "no-cache"
+    },
+    body: postData
+  })
+    .then(res => res.json())
+    .then(updatedSnack => {
+      const parentElement = document.querySelector(
+        `.snack[data-snackid="${updatedSnack._id}"]`
+      );
+
+      parentElement.querySelector("h1").textContent = updatedSnack.name;
+      parentElement.querySelector("h2").textContent = updatedSnack.taste;
+      parentElement.querySelector("p").textContent = updatedSnack.price;
+      parentElement.querySelector(".rating").textContent = updatedSnack.rating;
     });
 }
 
 function deleteSnack(id) {
-  fetch("https://frontend2019-7c5c.restdb.io/rest/snacks" + id, {
+  fetch("https://frontend2019-7c5c.restdb.io/rest/snacks/" + id, {
     method: "delete",
     headers: {
       "content-type": "application/json; charset=utf-8",
@@ -73,11 +157,6 @@ function deleteSnack(id) {
   })
     .then(res => res.json())
     .then(data => {
-      //TODO delete from dom
-      document.querySelector(`.snack[data-snackid="${id}"]`).remove();
+      // document.querySelector(`.snack[data-snackid="${id}"]`).remove();
     });
 }
-
-document.querySelector("button").addEventListener("click", e => {
-  post();
-});
