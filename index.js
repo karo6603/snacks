be97e36c2b76353ecf7a90edab0c104d6bd9763d
Karoline Krogsbøll <you@example.com>
2019-09-27 15:48:47 +0200

    delete

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function get() {
   fetch("https://frontend2019-7c5c.restdb.io/rest/snacks", {
     method: "get",
     headers: {
-      "Content-Type": "application/json; charset=utf-8",
+      "Content-Type": "application/json; charset=uft-8",
       "x-apikey": "5d887741fd86cb75861e260a",
       "cache-control": "no-cache"
     }
@@ -19,13 +19,22 @@ function addSnackToTheDOM(snack) {
   const template = document.querySelector("template").content;
   const copy = template.cloneNode(true);
 
+  copy.querySelector("article.snack").dataset.snackid = snack._id;
+
   copy.querySelector("h1").textContent = snack.name;
   copy.querySelector("h2").textContent = snack.taste;
   copy.querySelector("p").textContent = snack.price;
   copy.querySelector(".rating").textContent = snack.rating;
 
+  copy.querySelector("button").addEventListener("click", () => {
+    deleteSnack(snack._id);
+  });
+
   document.querySelector("#app").prepend(copy);
 }
+
+const form = document.querySelector("form");
+console.log(form.elements);
 
 function post() {
   const data = {
@@ -39,7 +48,7 @@ function post() {
   fetch("https://frontend2019-7c5c.restdb.io/rest/snacks", {
     method: "post",
     headers: {
-      "Content-Type": "application/json; charset=utf-8",
+      "content-type": "application/json; charset=utf-8",
       "x-apikey": "5d887741fd86cb75861e260a",
       "cache-control": "no-cache"
     },
@@ -50,6 +59,22 @@ function post() {
       console.log(data);
 
       addSnackToTheDOM(data);
+    });
+}
+
+function deleteSnack(id) {
+  fetch("https://frontend2019-7c5c.restdb.io/rest/snacks" + id, {
+    method: "delete",
+    headers: {
+      "content-type": "application/json; charset=utf-8",
+      "x-apikey": "5d887741fd86cb75861e260a",
+      "cache-control": "no-cache"
+    }
+  })
+    .then(res => res.json())
+    .then(data => {
+      //TODO delete from dom
+      document.querySelector(`.snack[data-snackid="${id}"]`).remove();
     });
 }
 
